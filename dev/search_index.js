3e65faa61da4f8b4f304127b5a72d9d50f86a37f
Documenter.jl <documenter@juliadocs.github.io>
2022-10-04 20:03:52 +0000

    build based on 193faf6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TheGraphAgents","category":"page"},{"location":"#TheGraphAgents","page":"Home","title":"TheGraphAgents","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TheGraphAgents.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TheGraphAgents]","category":"page"}]
+}
